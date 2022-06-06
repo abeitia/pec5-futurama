8e1369894aad56ecd34468e1e8aee9c6e333e25c
Agustin Beitia <abeitia@uoc.edu>
2022-06-05 20:01:11 -0500

    pwa

--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { RickMorty, Character, Episode} from '../models/rickandmorty.interface';
 import { Character } from '../models/futurama.interface';
 
 @Injectable({
@@ -12,17 +11,6 @@ export class CharactersService {
   constructor(private http: HttpClient) { }
 
 
-//   getAllCharacters(): Observable<RickMorty[]> {
-//     return this.http.get<RickMorty[]>('https://rickandmortyapi.com/api/character');
-//   }
-
-//  getCharacterById(id: string): Observable<Character> {
-//     return this.http.get<Character>('https://rickandmortyapi.com/api/character/' + id );
-//   }
-
-//  getEpisodes(ids: string): Observable<Episode[]> {
-//     return this.http.get<Episode[]>('https://rickandmortyapi.com/api/episode/' + ids );
-//   }
 
   getAllCharacters(): Observable<Character[]> {
     return this.http.get<Character[]>('https://api.sampleapis.com/futurama/characters');
